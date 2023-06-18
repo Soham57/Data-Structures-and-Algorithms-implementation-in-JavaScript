9e7874d3d119301ec8cfbb33100f6cec9e861a25
soham57 <sohammalviya65@gmail.com>
2023-06-18 15:58:44 +0530

    bubbleSort optimized

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,3 +1,8 @@
+/*This approach reduces the number of iterations and enhances the algorithm's efficiency.
+By keeping track of the number of swaps made in each pass, the algorithm can exit early if the array is already sorted.
+This eliminates unnecessary iterations, resulting in improved performance for partially sorted or nearly sorted arrays.
+The use of array destructuring assignment allows for a cleaner and more concise way to swap elements, making the code more readable. */
+
 function bubbleSort(arr) {
   let swapCount = 1;
 
