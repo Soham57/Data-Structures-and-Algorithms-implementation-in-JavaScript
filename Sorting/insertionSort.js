c3ca7d1fe47b098e981f89fe2a9301311828fd57
soham57 <sohammalviya65@gmail.com>
2023-06-19 10:10:23 +0530

    Insertion Sort cleaner implementation

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -2,12 +2,18 @@ function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let numToBeInserted = arr[i];
     let j;
-    for (j = i - 1; arr[j] > numToBeInserted && j > -1; j--) {
+
+    // Find the correct position to insert numToBeInserted
+    for (j = i - 1; j >= 0 && arr[j] > numToBeInserted; j--) {
       arr[j + 1] = arr[j];
     }
+
+    // Insert numToBeInserted at the correct position
     arr[j + 1] = numToBeInserted;
   }
+
   return arr;
 }
 
-insertionSort([1, 5, 2, 4, 3]);
+// Test the function
+console.log(insertionSort([1, 5, 2, 4, 3]));
