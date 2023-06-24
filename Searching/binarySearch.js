eea631c573289557a189327ee8ee3fc36effc41b
soham57 <sohammalviya65@gmail.com>
2023-06-24 20:26:04 +0530

    Binary search bug fix

--- Searching/binarySearch.js
+++ Searching/binarySearch.js
@@ -1,22 +1,19 @@
 function binarySearch(arr, element) {
-  if (arr.length === 0) {
-    return false;
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let midIndex = Math.floor((start + end) / 2);
+    if (arr[midIndex] === element) {
+      return true;
+    } else if (arr[midIndex] < element) {
+      start = midIndex + 1;
+    } else {
+      end = midIndex - 1;
+    }
   }
 
-  if (arr.length === 1) {
-    return arr[0] === element;
-  }
-
-  let midIndex = Math.floor(arr.length / 2);
-
-  if (arr[midIndex] === element) {
-    return true;
-  }
-
-  let left = arr.slice(0, midIndex);
-  let right = arr.slice(midIndex);
-
-  return binarySearch(left, element) || binarySearch(right, element);
+  return false;
 }
 
-console.log(binarySearch([0, 5, 10, 12, 15, 19, 21, 22, 24, 30], 10));
+console.log(binarySearch([0, 5, 10, 12, 15, 19, 21, 22, 24, 30], 102));
