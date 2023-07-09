4d08f2e1160c6bc8e5067aac01c0247f205d3597
soham57 <sohammalviya65@gmail.com>
2023-07-09 12:13:55 +0530

    showConnections method

--- Graph/graph.js
+++ Graph/graph.js
@@ -12,4 +12,17 @@ class Graph {
     this.adjacentList[node1].push(node2);
     this.adjacentList[node2].push(node1);
   }
+
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = " ";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
 }
