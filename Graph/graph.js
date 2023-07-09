1ac334e25b08f0e09bcf45daf579fcf0d1dde670
soham57 <sohammalviya65@gmail.com>
2023-07-09 12:22:24 +0530

    using graph

--- Graph/graph.js
+++ Graph/graph.js
@@ -26,3 +26,23 @@ class Graph {
     }
   }
 }
+
+// Create a new graph object
+const graph = new Graph();
+
+// Add vertices to the graph
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+
+// Add edges between vertices
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "D");
+graph.addEdge("D", "E");
+
+// Show the connections in the graph
+graph.showConnections();
