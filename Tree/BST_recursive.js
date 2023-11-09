f386ed1b7881aa395a587334b32dd673b786d11e
Soham57 <sohammalviya65@gmail.com>
2023-11-09 10:35:14 +0530

    Recursive implmenetation of insert and Remove BST node

---
+++ Tree/BST_recursive.js
@@ -0,0 +1,75 @@
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    search(target, root = this.root) {
+        if (target === null || target === undefined) {
+            throw new Error("Target cannot be null or undefined");
+        }
+
+        if (!root) {
+            return false;
+        }
+
+        if (target < root.val) {
+            return this.search(target, root.left);
+        } else if (target > root.val) {
+            return this.search(target, root.right);
+        } else {
+            return true;
+        }
+    }
+
+    insert(val, root = this.root) {
+        if (!root) {
+            return this.root = new Node(val);
+        }
+
+        if (root.val < val) {
+            root.right = this.insert(val, root.right);
+        } else if (root.val > val) {
+            root.left = this.insert(val, root.left);
+        }
+        return root;
+    }
+
+    remove(val, root = this.root) {
+        if (!root) {
+            return null;
+        }
+
+        if (val > root.val) {
+            root.right = this.remove(val, root.right);
+        } else if (val < root.val) {
+            root.left = this.remove(val, root.left);
+        } else {
+            if (root.left === null) {
+                return root.right;
+            } else if (root.right === null) {
+                return root.left;
+            } else {
+                let minNode = this.minValueNode(root.right);
+                root.val = minNode.val;
+                root.right = this.remove(minNode.val, root.right);
+            }
+        }
+        return root;
+    }
+
+    minValueNode(node) {
+        let current = node;
+        while (current.left !== null) {
+            current = current.left;
+        }
+        return current;
+    }
+}
