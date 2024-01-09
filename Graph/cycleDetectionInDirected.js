04499f584cc293bf3b5c647b50345bf48e76b84f
Soham57 <sohammalviya65@gmail.com>
2024-01-09 22:23:45 +0530

    Implemented cycle detection in Directed graph

--- Graph/cycleDetectionInDirected.js
+++ Graph/cycleDetectionInDirected.js
@@ -1,3 +1,37 @@
 function cycleDetectionInDirected(V, adjacencyList){
-    
+    let visitedNodes = new Set();
+    let inProgressNodes = new Set();
+
+    for(let node = 0; node < V; node++){
+        if(!visitedNodes.has(node)){
+            if(hasCycle(node, adjacencyList, visitedNodes, inProgressNodes))
+                return true;
+        }
+    }
+    return false;
 }
+
+function hasCycle(node, adjacencyList, visitedNodes, inProgressNodes){
+    if(inProgressNodes.has(node)){
+        return true;
+    }
+    
+    if(visitedNodes.has(node)){
+        return false;
+    }
+    
+    visitedNodes.add(node);
+    inProgressNodes.add(node);
+
+    let neighbors = adjacencyList[node];
+
+    for(let nextNode of neighbors){
+        if(!visitedNodes.has(nextNode)){
+            if(hasCycle(nextNode, adjacencyList, visitedNodes, inProgressNodes))
+                return true;
+        }
+    }
+    inProgressNodes.delete(node);
+    return false;
+}
+
