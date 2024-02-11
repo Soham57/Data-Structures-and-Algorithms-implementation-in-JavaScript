c373cd02b8feec4b525839fb19ab5cd453fbb688
Soham Malviya <42260390+Soham57@users.noreply.github.com>
2024-02-11 07:41:50 +0530

    Update unionFind.js
    Implementing Union Find Algorithm

--- Graph/unionFind.js
+++ Graph/unionFind.js
@@ -1,1 +1,41 @@
+class UnionFind {
+  constructor() {
+    this.parents = {};
+    this.ranks = {}
+  }
 
+  createSet(value) {
+    this.parents[value] = value;
+    this.ranks[value] = 0;
+  }
+
+  find(value) {
+    if(!(value in this.parents)) return null;
+
+    if(value !== this.parents[value]){
+      this.parents[value] = this.find(this.parents[value]);
+    }
+
+    return this.parents[value];
+  }
+
+  union(valueOne, valueTwo) {
+    if(!(valueOne in this.parents) || !(valueTwo in this.parents)) return null;
+
+    const valueOneRoot = this.find(valueOne);
+    const valueTwoRoot = this.find(valueTwo);
+
+    if(this.ranks[valueOneRoot] < this.ranks[valueTwoRoot]){
+      this.parents[valueOneRoot] = valueTwoRoot;
+    }
+
+    else if(this.ranks[valueTwoRoot] < this.ranks[valueOneRoot]){
+      this.parents[valueTwoRoot] = valueOneRoot;
+    }
+
+    else{
+      this.parents[valueTwoRoot] = valueOneRoot;
+      this.ranks[valueOneRoot]++;
+    }
+  }
+}
