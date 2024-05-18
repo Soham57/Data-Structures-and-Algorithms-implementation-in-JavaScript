71c5d149efbe51c1b75afb0863d981b9d35ad9c4
Soham Malviya <42260390+Soham57@users.noreply.github.com>
2024-05-18 06:30:11 +0530

    Bug Fix
    Length variable introduced

--- Queue/queue_usingMap.js
+++ Queue/queue_usingMap.js
@@ -3,24 +3,31 @@ class Queue {
       this.elements = {};
       this.head = 0;
       this.tail = 0;
+      this.length = 0;
     }
     enqueue(element) {
       this.elements[this.tail] = element;
       this.tail++;
+      this.length++;
     }
     dequeue() {
+      if(this.length === 0){
+          return null;
+      }
+      
       const item = this.elements[this.head];
       delete this.elements[this.head];
       this.head++;
+      this.length--;
       return item;
     }
     peek() {
       return this.elements[this.head];
     }
-    get length() {
-      return this.tail - this.head;
+    getLength() {
+      return this.length;
     }
-    get isEmpty() {
+    isEmpty() {
       return this.length === 0;
     }
   }
